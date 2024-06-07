e4ebd8b004508ae6fad54ed8bf014d37dd3ef78f
Panagiotis Xynos <panos@onlyascii.io>
2024-06-07 16:47:14 +0100

    Add Godot app

---
+++ install/optional/app-godot.sh
@@ -0,0 +1,13 @@
+cat <<EOF >~/.local/share/applications/Godot.desktop
+[Desktop Entry]
+Version=4.2.2
+Name=Godot
+Comment=Your free, open‑source game engine
+Exec=Godot_v4.2.2-stable_linux.x86_64
+Terminal=false
+Type=Application
+Icon=/home/$USER/.local/share/omakub/install/optional/icons/Godot.png
+Categories=GTK;
+MimeType=text/html;text/xml;application/xhtml_xml;
+StartupNotify=true
+EOF
